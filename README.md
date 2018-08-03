e1a9eff7fd53aef44eafc60d507c2b6336e1da62
Jürg Lehni <juerg@scratchdisk.com>
2018-08-03 10:22:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# vue-cli-issue-sass-import-css
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Demonstrating the broken Sass import of a CSS file.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="sass">
+@import '~hint.css/hint.base.css'
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
